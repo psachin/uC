ba2d20ae3fcb050633550d6065b8fbcec66dd7a4
psachin <iclcoolster@gmail.com>
2014-01-11 00:37:06 +0530

    Subject: Using button.
    Description:
    
    - Video 11

--- led.c
+++ led.c
@@ -8,7 +8,7 @@
 
 void main(void) {
   /* DDR: Data direction register. It tells the uC whether to make a
-     port input or outoup. We'll make PORT B as o/p */
+     port input or output. We'll make PORT B as o/p */
 
   /* set DDRB to its previous value OR make PORT B(PIN 0) as o/p */
   // DDRB |= 0b00000001;		/* last digit is for PIN 0 */
@@ -21,13 +21,24 @@ void main(void) {
 				   as 1. PINB0 has reference in 'io.h'
 				   file */
 
+  DDRB &= ~(1 << PINB1);	/* DDR input PINB1 */
+  PORTB |= 1 << PINB1;		/* set PINB1 to HIGH */
+
   while (1) {
     /* never ending loop. */
     PORTB ^= 1 << PINB0;		/* '^' is X-OR operation
 					   which will flip PIN0 to 1
 					   and 0 */
-    _delay_ms(1000);			/* delay 100 milli seconds */
-   }
+
+    if (bit_is_clear(PINB, 1)) {
+      /* button is pressed */
+      _delay_ms(10000);
+    }
+    else {
+      /* button is not pressed */
+      _delay_ms(1000);			/* delay 100 milli seconds */
+    }
+  }
 }
 
 
